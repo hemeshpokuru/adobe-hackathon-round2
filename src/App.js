b242fa285ce82254109139df50f1b6ecb665e75e
hemeshpokuru <hemeshpokuru17@gmail.com>
2025-07-28 07:57:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import PdfViewer from './PdfViewer';
 import OutlineSidebar from './OutlineSidebar';
 import PersonaSelector from './PersonaSelector';
 import KeywordHighlighter from './KeywordHighlighter';
-import PDFUpload from './PDFUpload';
 import './App.css';
 
 function App() {
   const [persona, setPersona] = useState("");
-  const [uploadedPDF, setUploadedPDF] = useState(null);
 
   return (
     <div className="app-container">
       <div>
-        <PDFUpload setUploadedPDF={setUploadedPDF} />
         <PersonaSelector persona={persona} setPersona={setPersona} />
         <KeywordHighlighter persona={persona} />
-        {uploadedPDF && <OutlineSidebar />}
+        <OutlineSidebar />
       </div>
-      <PdfViewer uploadedPDF={uploadedPDF} />
+      <PdfViewer />
     </div>
   );
 }

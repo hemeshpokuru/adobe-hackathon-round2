5fbdf8fe8f911c9ca3c402a846f979bcab24bebf
hemeshpokuru <hemeshpokuru17@gmail.com>
2025-07-28 14:15:02 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import PdfViewer from './PdfViewer';
 import OutlineSidebar from './OutlineSidebar';
 import PersonaSelector from './PersonaSelector';
-import KeywordHighlighter from './KeywordHighlighter';
 import './App.css';
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
     <div className="app-container">
       <div>
         <PersonaSelector persona={persona} setPersona={setPersona} />
-        <KeywordHighlighter persona={persona} />
         <OutlineSidebar />
       </div>
       <PdfViewer />
--- src/PdfViewer.js
+++ src/PdfViewer.js
@@ -1,30 +1,25 @@
 import React, { useEffect } from 'react';
 
-const PdfViewer = ({ uploadedPDF }) => {
+const PdfViewer = () => {
   useEffect(() => {
-    if (!uploadedPDF) return;
+    const adobeDCView = new window.AdobeDC.View({
+      clientId: "4c9fb13580e3496db2eb7faee352be41",
+      divId: "adobe-dc-view"
+    });
 
-    const showPDF = () => {
-      const adobeDCView = new window.AdobeDC.View({
-        clientId: "4c9fb13580e3496db2eb7faee352be41",
-        divId: "adobe-dc-view"
-      });
-
-      adobeDCView.previewFile({
-        content: {
-          promise: Promise.resolve(uploadedPDF.base64)
-        },
-        metaData: {
-          fileName: uploadedPDF.file.name
+    adobeDCView.previewFile({
+      content: {
+        location: {
+          url: "sample.pdf"
         }
-      }, { embedMode: "SIZED_CONTAINER" });
-    };
+      },
+      metaData: {
+        fileName: "sample.pdf"
+      }
+    });
+  }, []);
 
-    if (window.AdobeDC) showPDF();
-    else document.addEventListener("adobe_dc_view_sdk.ready", showPDF);
-  }, [uploadedPDF]);
-
-  return <div id="adobe-dc-view" style={{ height: "600px", width: "100%" }}></div>;
+  return <div id="adobe-dc-view" style={{ height: '800px' }}></div>;
 };
 
 export default PdfViewer;
